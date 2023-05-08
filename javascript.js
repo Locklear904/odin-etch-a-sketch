ae44d2c761f691d831e3205e6eea448c8fefeeb7
Rich Barnes <locklear904@gmail.com>
2023-05-08 10:19:15 -0400

    Restructure code to allow flexible grid size
    Change code to make columns and fill the columns with grid boxes
    based on the size input into the createGrid function. Add CSS hover
    to the grid boxes and commented out javascript event listener for now.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,22 @@
 const grid = document.querySelector('#sketchContainer');
 
+
+
 function createGrid(size) {
-    for (let i = 0; i < (size * size); i++) {
-        let square = document.createElement('div');
-        square.classList.add('gridSquare');
-        grid.appendChild(square);
-        square.addEventListener("mouseover", function() {
-            square.style.backgroundColor = "black";
-        })
+    for (let i = 0; i < size; i++) {
+        let column = document.createElement('div');
+        column.classList.add('gridColumn');
+        grid.appendChild(column);
+        for (let i = 0; i < size; i++) {
+            let box = document.createElement('div');
+            box.classList.add('gridBox');
+            column.appendChild(box);
+            //Using CSS hover, but saving event listener just in case
+            /*box.addEventListener("onmouseover", function() {
+                box.style.backgroundColor = "black";
+            });*/
+        }
     }
 }
 
-createGrid(4);
+createGrid(10);
