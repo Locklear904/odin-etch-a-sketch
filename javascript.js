570a964f876500fe5d45120ceb56648f99e0ecf2
Rich Barnes <locklear904@gmail.com>
2023-05-08 13:56:49 -0400

    Add functionality to create grid with button
    Add click event listener to button that prompts a number 1-100
    and uses that as the size input for the createGrid function. It also
    removes the previous grid when a proper input is given.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,18 @@
 const grid = document.querySelector('#sketchContainer');
+const gridBtn = document.querySelector('#gridBtn');
 
-
+function newGrid() {
+    let size = prompt("Please enter a number from 1-100");
+    size = Math.round(size);
+    if (size <= 100 && size >= 1) {
+        while (grid.firstChild) {
+            grid.removeChild(grid.firstChild);
+        }
+        createGrid(size);
+    }   else {
+        alert("Please try again! Make sure to enter a number from 1-100.");
+    }
+}
 
 function createGrid(size) {
     for (let i = 0; i < size; i++) {
@@ -19,4 +31,5 @@ function createGrid(size) {
     }
 }
 
-createGrid(10);
+gridBtn.addEventListener("click", newGrid);
+createGrid(16);
